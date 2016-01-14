a5a384942f41b0b5148d4574f07305c8808188ee
Farrow <farrowc@msoe.edu>
2016-01-14 13:47:58 -0600

    Finished up command line command and main method stub.  Command line syntax is 'brady-tool compare project1 project2'

--- src/brady-tool.js
+++ src/brady-tool.js
@@ -1,10 +1,27 @@
+/*
+ * Command line application that compares the dependency versions of two projects
+ */
 
 var commander = require('commander');
 
-commmander
-	.version('1.0.0')
-	.command('compare [fileOne] [fileTwo]', 'Compare the dependencies of two projects');
-	
+function compare(fileOne, fileTwo){
+	//Method Stub for main compare method
+}
+
+
+
+
+//Commander lines go below this comment
+//All commands need a command, description, alias, and action component
+
+commander
+	.version('1.0.0');
+
+commander
+	.command('compare [fileOne] [fileTwo]')
+		.description('Compare the dependencies of two projects')
+		.alias("cmp")
+		.action(compare);
 	
 
 commander.parse(process.argv);
