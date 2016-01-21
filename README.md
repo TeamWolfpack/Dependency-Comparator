54f8fed612fe47cd058c40569fa4b226930aeb76
Jimmy Kuczynski <Jimmy Kuczynski>
2016-01-21 14:48:17 -0600

    bradytool-68 created read.md for npm

---
+++ README.md
@@ -0,0 +1,5 @@
+Brady Dependency Tool
+
+Team wolfpack has been working very hard to develop a Node.js tool
+for Brady Corp. The tool is meant to help the development team at
+Brady manage their node modules with ease.
--- gulpfile.js
+++ gulpfile.js
@@ -112,3 +112,65 @@ var runMochaTest = function(files, timeoutInMillis, exitOnError, done) {
         }
     });
 };
+
+gulp.task('npmPublish', function (callback) {
+    var username = "bradyteamstark";
+    var password = "1PddAQLjXpWP";
+    var email = "kuczynskij@msoe.edu";
+    if (!username) {
+        var usernameError = new Error("Username is required as an argument --username exampleUsername");
+        return callback(usernameError);
+    }
+    if (!password) {
+        var passwordError = new Error("Password is required as an argument --password  examplepassword");
+        return callback(passwordError);
+    }
+    if (!email) {
+        var emailError = new Error("Email is required as an argument --email example@email.com");
+        return callback(emailError);
+    }
+	console.log("made it here");
+    var uri = "http://registry.npmjs.org/";
+    npm.load(null, function (loadError) {
+        if (loadError) {
+            return callback(loadError);
+        }
+        var auth = {
+            username: username,
+            password: password,
+            email: email,
+            alwaysAuth: true
+        };
+        var addUserParams = {
+            auth: auth
+        };
+        npm.registry.adduser(uri, addUserParams, function (addUserError, data, raw, res) {
+            if (addUserError) {
+                return callback(addUserError);
+            }
+            var metadata = require('./package.json');
+            metadata = JSON.parse(JSON.stringify(metadata));
+            npm.commands.pack([], function (packError) {
+                if (packError) {
+                    return callback(packError);
+                }
+                var fileName = metadata.name + '-' + metadata.version + '.tgz';
+                var bodyPath = require.resolve('./' + fileName);
+                var body = fs.createReadStream(bodyPath);
+                var publishParams = {
+                    metadata: metadata,
+                    access: 'public',
+                    body: body,
+                    auth: auth
+                };
+                npm.registry.publish(uri, publishParams, function (publishError, resp) {
+                    if (publishError) {
+                        return callback(publishError);
+                    }
+                    console.log("Publish succesfull: " + JSON.stringify(resp));
+                    return callback();
+                });
+            })
+        });
+    });
+});
