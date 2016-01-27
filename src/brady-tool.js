52c0f2019a6693291ec3e9c1e2f0d539a5dad146
Farrow <farrowc@msoe.edu>
2016-01-27 11:54:07 -0600

    Now store the maxinstances of unmatched deps in project 2 as a number.

--- src/brady-tool.js
+++ src/brady-tool.js
@@ -271,7 +271,7 @@ function compareAndMatch(projectOne, projectTwo) {
             }
             dependencies[dependencies.length] = {
                 name: dep,
-                maxinstances: projectTwoDep[dep],
+                maxinstances: projectTwoDep[dep].length,
                 instances: matchedDeps,
             };
         }
