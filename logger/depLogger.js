0b7d29e50985bfc5c34fbcaf0941585192385095
unknown <farrowc@MSOE-CND52919NQ.msoe.edu>
2016-02-18 15:57:47 -0600

    Logger now creates a directory if it does not exist

--- logger/depLogger.js
+++ logger/depLogger.js
@@ -4,6 +4,8 @@
 
 var path = require("path");
 var jsonfile = require("jsonfile");
+var mkdirp = require('mkdirp');
+
 
 module.exports = {
     logDependencies: logDependencies
@@ -18,6 +20,12 @@ function logDependencies(dependencies,outputFile){
     var fileName = __dirname+"/../logfiles/"+dateString;
     var fileName = path.normalize(fileName)+".json";
     var depString = JSON.stringify(dependencies);
+    mkdirp('__dirname+"/../logfiles', function (err) {
+        if (err){
+            console.log("An error has occured with the"+
+                "logger:\n"+err);
+        }
+    });
     jsonfile.writeFile(fileName,dependencies,{spaces:1},function(err){
         if(err!=null) {
             console.log("An error has occured with the"+
