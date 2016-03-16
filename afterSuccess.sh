642f16b98236a737f884d0b6f2af272337533c77
aaronchun7 <aaronchun7@gmail.com>
2016-03-16 14:44:08 -0500

    trying to test out travis handlig tags on commits and pushes

--- afterSuccess.sh
+++ afterSuccess.sh
@@ -6,9 +6,16 @@ gulp supportMac
 # what to do when the build and tests succeed on dev
 if [ "$TRAVIS_BRANCH" == "dev" ]; then
 	gulp
+	if [ "$TRAVIS_TAG"]; then
+		echo $TRAVIS_TAG
+	fi
+	#gulp bumpPatchBeta
+	#gulp publish
 	echo "ran in dev"
 # what to do when the build and tests succeed on master
 elif [ "$TRAVIS_BRANCH" == "master" ]; then
 	gulp
+	#gulp bumpMinor
+	#gulp bumpMajor
 	echo "ran in master"
 fi
