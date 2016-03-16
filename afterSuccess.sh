a3435da656320a4085ec0ac1a18ac2f33c887757
aaronchun7 <aaronchun7@gmail.com>
2016-03-16 12:25:30 -0500

    added gulp supportMac to travis.yml file

--- afterSuccess.sh
+++ afterSuccess.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
+# start by making it supported on Mac
+gulp supportMac
+
+# what to do when the build and tests succeed on dev
 if [ "$TRAVIS_BRANCH" == "dev" ]; then
 	gulp
 	echo "ran in dev"
-fi
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+# what to do when the build and tests succeed on master
+elif [ "$TRAVIS_BRANCH" == "master" ]; then
 	gulp
 	echo "ran in master"
 fi
