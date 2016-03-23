88d5ea2d56a672505fbbe6421b1ae52c751b57db
Farrow <farrowc@msoe.edu>
2016-03-23 08:47:44 -0500

    fixed the tests

--- test/unit/color-test.js
+++ test/unit/color-test.js
@@ -18,40 +18,49 @@ describe("Color Tests", function() {
         });
     });
     describe("colorVersion Tests", function(){
-        it("Should give the up to date color when a instance is up to date", function() {
+        it("Should give the up to date color when an instance is up to date", function() {
             var colors = require("../../modules/colors");
             var instance = {
                 version: "poopInstance",
                 color: "upToDate"
             };
-            var versionedInstance = colors.colorScheme.upToDate(instance);
+            var versionedInstance = colors.colorScheme.upToDate(instance.version);
             assert.equal(colors.colorVersion(instance),versionedInstance);
         });
-        it("Should give the patch update color when a instance is behind by a patch", function() {
+        it("Should give the patch update color when an instance is behind by a patch", function() {
             var colors = require("../../modules/colors");
             var instance = {
                 version: "poopInstance",
                 color: "patch"
             };
-            var versionedInstance = colors.colorScheme.patch(instance);
+            var versionedInstance = colors.colorScheme.patch(instance.version);
             assert.equal(colors.colorVersion(instance),versionedInstance);
         });
-        it("Should give the minor update color when a instance is behind by a minor update", function() {
+        it("Should give the minor update color when an instance is behind by a minor update", function() {
             var colors = require("../../modules/colors");
             var instance = {
                 version: "poopInstance",
                 color: "minor"
             };
-            var versionedInstance = colors.colorScheme.minor(instance);
+            var versionedInstance = colors.colorScheme.minor(instance.version);
             assert.equal(colors.colorVersion(instance),versionedInstance);
         });
-        it("Should give the major update color when a instance is behind by a major update", function() {
+        it("Should give the major update color when an instance is behind by a major update", function() {
             var colors = require("../../modules/colors");
             var instance = {
                 version: "poopInstance",
                 color: "major"
             };
-            var versionedInstance = colors.colorScheme.major(instance);
+            var versionedInstance = colors.colorScheme.major(instance.version);
+            assert.equal(colors.colorVersion(instance),versionedInstance);
+        });
+        it("Should give the unmatched color when an instance is unmatched", function() {
+            var colors = require("../../modules/colors");
+            var instance = {
+                version: "poopInstance",
+                color: "unmatched"
+            };
+            var versionedInstance = colors.colorScheme.unmatched(instance.version);
             assert.equal(colors.colorVersion(instance),versionedInstance);
         });
     });
