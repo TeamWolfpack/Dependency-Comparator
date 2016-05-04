b7af86224ed0603bcaa0dace15c6d8204ec171a8
aaronchun7 <aaronchun7@gmail.com>
2016-05-04 08:53:03 -0500

    changed directios to hovering over table cells gives the column and row information

--- html/htmlTableCreator.js
+++ html/htmlTableCreator.js
@@ -1,10 +1,15 @@
+//var tableHeaderText = "";
+//var tableBodyText = "";
 var tableText = "";
-var headers = "<tr>";
+
+var headers = "<thead><tr>";
 for (var header in table.options.head) {
 	headers += "<th>" + table.options.head[header] + "</th>";
 }
-headers += "</tr>";
-tableText += headers;
+headers += "</tr></thead>";
+//tableHeaderText += headers;
+//tableBodyText += "<tbody>";
+tableText += headers + "<tbody>";
 
 var rowCount = table.length - 1;
 for (var r = 0; r < rowCount; r++) {
@@ -16,34 +21,71 @@ for (var r = 0; r < rowCount; r++) {
         var npmVersion = row[1].version;
         var npmColor = row[1].color;
 
-        rowString += "<td>" + depName + "</td>";
-        rowString += "<td class=\"" + npmColor + "\">" + npmVersion + "</td>";
+        rowString += "<td class=\"filterable-cell\">" + depName + "</td>";
+        rowString += "<td class=\"filterable-cell " + npmColor + "\">" + npmVersion + "</td>";
 
         for (var c = 2; c < row.length; c += 2) {
             var version = row[c] ? row[c].version : "";
             var color = row[c] ? row[c].color : "";
 
             var path = row[c + 1];
-            rowString += "<td class=\"" + color + "\">" + version + "</td>";
-            rowString += "<td>" + path + "</td>";
+            rowString += "<td class=\"filterable-cell " + color + "\">" + version + "</td>";
+            rowString += "<td class=\"filterable-cell\">" + path + "</td>";
 
         }
     }
     else{
 
-        rowString += "<td>" + "</td>" + "<td>" + "</td>";
+        rowString += "<td class=\"filterable-cell\">" + "</td>" + "<td class=\"filterable-cell\">" + "</td>";
 
         for (var c = 1; c < row.length; c += 2) {
             var version = row[c] ? row[c].version : "";
             var color = row[c] ? row[c].color : "";
 
             var path = row[c + 1];
-            rowString += "<td class=\"" + color + "\">" + version + "</td>";
+            rowString += "<td class=\"filterable-cell " + color + "\">" + version + "</td>";
             rowString += "<td>" + path + "</td>";
 
         }
     }
 	rowString += "</tr>";
-	tableText += rowString;
+    //tableBodyText += rowString;
+    tableText += rowString;
 }
-htmlTable.innerHTML = tableText;
+//tableBodyText += "</tbody>";
+tableText += "</tbody>";
+
+////freeze pane
+//var updateFreezePane = function() {
+//    var tableHeader = $("#tableHeader");
+//    var tableBody = $("#tableBody");
+//    var tableOffset = tableBody.offset().top;
+//    var tableWidth = tableBody.width();
+//    var offset = $(this).scrollTop();
+//    var header = $("#tableHeader > thead");
+//
+//    if (offset >= 0) {
+//        tableHeader.show();
+//
+//        var tableHeaderThs = tableHeader.find('tr > th');
+//
+//        //dynamically copy the width of each original th to the fixed header th
+//
+//        $.each(header.find('tr > th'), function(ind, val){
+//            var originalWidth = $(val).width();
+//            $(tableHeaderThs[ind]).width(originalWidth);
+//        });
+//    } else {
+//        tableHeader.hide();
+//    }
+//};
+//
+//$(window).bind("scroll", updateFreezePane);
+//$(window).bind("resize", updateFreezePane);
+
+/*** hover over element to show info for cell ***/
+//function getCellInfo()
+
+//tableBody.innerHTML = tableBodyText;
+//tableHeader.innerHTML = tableHeaderText;
+.innerHTML = tableText;
