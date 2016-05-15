97cede1b3bde3145c01904681eba7c8cb86ebe3b
unknown <leonardj@msoe.edu>
2016-05-15 15:59:00 -0500

    comment out line that starts server

--- bin/brady-tool.js
+++ bin/brady-tool.js
@@ -190,7 +190,7 @@ function compareProjects(projects) {
             }
             summarizer.printSummaryTable();
         }
-        server.start(tableCopy, commander.depth);
+        //server.start(tableCopy, commander.depth);
     });
 }
 
