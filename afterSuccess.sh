6cb1ab9f78e7ce4837782142426dd0eedce43f3d
aaronchun7 <aaronchun7@gmail.com>
2016-05-16 09:17:14 -0500

    changed how publishing with tags work

--- afterSuccess.sh
+++ afterSuccess.sh
@@ -4,12 +4,12 @@
 gulp supportMac
 
 # tags will determine what kind of update we need (Major, Minor, Patch, Beta)
-if [ "$TRAVIS_TAG" == "major" ]; then
+if [[ "$TRAVIS_TAG" == *"major"* ]]; then
 	gulp bumpMajor
-elif [ "$TRAVIS_TAG" == "minor" ]; then
+elif [[ "$TRAVIS_TAG" == *"minor"* ]]; then
 	gulp bumpMinor
-elif [ "$TRAVIS_TAG" == "patch" ]; then
+elif [[ "$TRAVIS_TAG" == *"patch"* ]]; then
 	gulp bumpPatch
-elif [ "$TRAVIS_TAG" == "beta" ]; then
+elif [[ "$TRAVIS_TAG" == *"beta"* ]]; then
 	gulp bumpBeta
 fi
